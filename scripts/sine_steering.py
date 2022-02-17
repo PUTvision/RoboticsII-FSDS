0c851cd6d2e1beb3f90b9089b97e7aecf8bdaab6
Mateusz <piechocki.mateusz97@gmail.com>
2022-02-17 16:08:45 +0100

    Update script

--- scripts/sine_steering.py
+++ scripts/sine_steering.py
@@ -1,27 +1,37 @@
 #!/usr/bin/env python3
+
 import math
-import rospy
+
+import rospy as rp
+
 from fs_msgs.msg import ControlCommand
 from fs_msgs.msg import FinishedSignal
 
 
 class SineSteering:
     def __init__(self, steering_period: int = 10, publishing_frequency: float = 5.0, throtle: float = 0.2):
-        self.t = 0.0                                        # Steering timestamp.
+        # Steering timestamp.
+        self.t = 0.0
 
-        self.steering_period = steering_period              # The number of seconds to do a full steering cycle (neutral -> full right -> full left -> neutroal).
-        self.publishing_frequency = publishing_frequency    # Hz at what the car setpoints are published.
-        self.throtle = throtle                              # The throtle. This is just a static value.
+        # The number of seconds to do a full steering cycle (neutral -> full right -> full left -> neutral).
+        self.steering_period = steering_period
 
-        self.controll_publisher = rospy.Publisher('/fsds/control_command', ControlCommand, queue_size=10)
-        self.finished_publisher = rospy.Publisher('/fsds/signal/finished', FinishedSignal, queue_size=1)
+        # Hz at what the car setpoints are published.
+        self.publishing_frequency = publishing_frequency
+        
+        # The throtle. This is just a static value.
+        self.throtle = throtle
 
-    def publish(self, x):
+        self.controll_publisher = rp.Publisher('/fsds/control_command', ControlCommand, queue_size=10)
+        self.finished_publisher = rp.Publisher('/fsds/signal/finished', FinishedSignal, queue_size=1)
+
+
+    def publish(self, *args):
         # TODO: Implement sine signal steering value calculation
         steering = 0
 
         cc = ControlCommand()
-        cc.header.stamp = rospy.Time.now()
+        cc.header.stamp = rp.Time.now()
         cc.throttle = self.throtle
         cc.steering = steering
         cc.brake = 0
@@ -30,16 +40,17 @@ class SineSteering:
         # TODO: Update steering timestamp 
         self.t += 0.0
 
-    def finish(self, x):
+
+    def finish(self, *args):
         self.finished_publisher.publish(FinishedSignal())
 
 
 if __name__ == '__main__':
-    rospy.init_node('sine_steering')
+    rp.init_node('sine_steering')
 
     sine_steering = SineSteering()
 
-    rospy.Timer(rospy.Duration(0.1), sine_steering.publish)
-    rospy.Timer(rospy.Duration(30), sine_steering.finish)
+    rp.Timer(rp.Duration(0.1), sine_steering.publish)
+    rp.Timer(rp.Duration(30), sine_steering.finish)
 
-    rospy.spin()
+    rp.spin()
